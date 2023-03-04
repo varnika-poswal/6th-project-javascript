1620983d4510d0ec75d6d54c403d165206dd145f
varnika-poswal <123875147+varnika-poswal@users.noreply.github.com>
2023-03-04 10:36:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    var scroll=document.body.scrollTop || document.documentElement.scrollTop;
+    var height=document.documentElement.scrollHeight-document.documentElement.clientHeight;
+    var scrolled=(scroll/height)*100;
+    document.getElementById("indicator").style.width=scrolled +"%";
+}
+window.onscroll=function(){
+    this.myFunction();
+}
